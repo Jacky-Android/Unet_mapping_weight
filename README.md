45dda7f0d62a2e2e672900af3b39bbee33e0e491
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2023-01-14 11:17:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Unet_mapping_weight
+# U-Net(Convolutional Networks for Biomedical Image Segmentation)
+
+## 该项目主要参考以下开源仓库
+* [https://github.com/milesial/Pytorch-UNet](https://github.com/milesial/Pytorch-UNet)
+* [https://github.com/pytorch/vision](https://github.com/pytorch/vision)
+## 拥有unet版本：
+* [unet](https://github.com/Jacky-Android/unet-torch-/blob/main/src/unet.py)
+* [vgg16unet](https://github.com/Jacky-Android/unet-torch-/blob/main/src/vgg_unet.py)
+* [convnext_unet](https://github.com/Jacky-Android/unet-torch-/blob/main/src/unet_convnext.py)
+* [segnet](https://github.com/Jacky-Android/unet-torch-/blob/main/src/segnet.py)
+* [unet_cbam](https://github.com/Jacky-Android/unet-torch-/blob/main/src/unet_sc.py)
+* [unet_se](https://github.com/Jacky-Android/unet-torch-/blob/main/src/unet_selayer.py)
+* [mobilenet_uet](https://github.com/Jacky-Android/unet-torch-/blob/main/src/mobilenet_unet.py)
+* [unet_networks(unet_attention,R2UNet,R2UNet_attention)](https://github.com/Jacky-Android/unet-torch-/blob/main/src/unet_networks.py)
+* [unet_eca](https://github.com/Jacky-Android/unet-torch-/blob/main/src/unet_eca.py)
+## 环境配置：
+* Python3.6/3.7/3.8
+* Pytorch1.10
+* Ubuntu或Centos(Windows暂不支持多GPU训练)
+* 最好使用GPU训练
+* 详细环境配置见`requirements.txt`
+## unet的提出，但是unet还有很大的改进可能
+![image](https://img-blog.csdnimg.cn/20210316213927771.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80NTA3NDU2OA==,size_16,color_FFFFFF,t_70)
+## 文件结构：
+```
+  ├── src: 搭建U-Net模型代码
+  ├── train_utils: 训练、验证以及多GPU训练相关模块
+  ├── my_dataset.py: 自定义dataset用于读取DRIVE数据集(视网膜血管分割)
+  ├── train.py: 以单GPU为例进行训练
+  ├── train_multi_GPU.py: 针对使用多GPU的用户使用
+  ├── predict.py: 简易的预测脚本，使用训练好的权重进行预测测试
+  └── compute_mean_std.py: 统计数据集各通道的均值和标准差
+```
+
---
+++ my_dataset.py
@@ -0,0 +1,63 @@
+import os
+from PIL import Image
+import numpy as np
+from torch.utils.data import Dataset
+import torch 
+
+
+class DriveDataset(Dataset):
+    def __init__(self, root: str, seg:str,train: bool, transforms=None):
+        super(DriveDataset, self).__init__()
+        self.flag = "images"
+        self.ann = "ann"
+        self.wt = "wt"
+        self.transforms = transforms
+        img_names = [i for i in os.listdir(os.path.join(root+self.flag , seg)) if i.endswith(".jpg")]
+        self.img_list = [os.path.join(root+self.flag , seg, i) for i in img_names]
+        
+        # check files
+    
+
+        self.roi_mask = [os.path.join(root+self.ann, seg, i[:-4]+'.png')
+                         for i in img_names]
+        self.wt_list = [os.path.join(root+self.wt, seg, i[:-4]+'.npy')for i in img_names]
+        # check files
+        for i in self.roi_mask:
+            if os.path.exists(i) is False:
+                raise FileNotFoundError(f"file {i} does not exists.")
+
+    def __getitem__(self, idx):
+        img = Image.open(self.img_list[idx]).convert('RGB')
+        mask = Image.open(self.roi_mask[idx])
+        mask_wt = torch.tensor(np.load(self.wt_list[idx]).astype(float))
+        
+        if torch.max(mask_wt) > 0:
+            mask_wt = mask_wt / torch.max(mask_wt)
+        
+        # 这里转回PIL的原因是，transforms中是对PIL数据进行处理
+        #mask = Image.fromarray(np.array(mask.long()))
+
+        if self.transforms is not None:
+            img,mask= self.transforms(img,mask)
+
+        return img, mask,mask_wt
+
+    def __len__(self):
+        return len(self.img_list)
+
+    @staticmethod
+    def collate_fn(batch):
+        images, targets,wt = list(zip(*batch))
+        batched_imgs = cat_list(images, fill_value=0)
+        batched_targets = cat_list(targets, fill_value=255)
+        batched_wt = cat_list(wt, fill_value=255)
+        return batched_imgs, batched_targets,batched_wt
+
+
+def cat_list(images, fill_value=0):
+    max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
+    batch_shape = (len(images),) + max_size
+    batched_imgs = images[0].new(*batch_shape).fill_(fill_value)
+    for img, pad_img in zip(images, batched_imgs):
+        pad_img[..., :img.shape[-2], :img.shape[-1]].copy_(img)
+    return batched_imgs
