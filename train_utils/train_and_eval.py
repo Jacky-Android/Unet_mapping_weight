93e90e80e2e44f5a9efdf949e616041885f148ac
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2023-01-16 15:51:33 +0800

    Update train_and_eval.py

--- train_utils/train_and_eval.py
+++ train_utils/train_and_eval.py
@@ -11,12 +11,12 @@ def criterion(inputs, target, loss_weight=None, num_classes: int = 2, dice: bool
         # 
         loss = torch.nn.CrossEntropyLoss(reduce=False)(x, target)
        
-        weight = get_weights(target, 10, 5, [320,320])
+        weight = get_weights(target, 10, 5, target.shape)
         loss = loss.cuda()*weight.cuda()
-        loss = torch.mean(loss)*0.5
+        loss = torch.mean(loss)
         if dice is True:
             dice_target = build_target(target, num_classes, ignore_index)
-            loss += dice_loss(x, dice_target, multiclass=True, ignore_index=ignore_index)*0.5
+            loss += dice_loss(x, dice_target, multiclass=True, ignore_index=ignore_index)
         losses[name] = loss
 
     if len(losses) == 1:
