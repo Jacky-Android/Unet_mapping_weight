dfc011266a53f4652cea7a9e23ba6ca28a2a8efc
Jacky-Android <55181594+Jacky-Android@users.noreply.github.com>
2023-01-16 14:36:12 +0800

    Update train_and_eval.py

--- train_utils/train_and_eval.py
+++ train_utils/train_and_eval.py
@@ -16,7 +16,7 @@ def criterion(inputs, target, loss_weight=None, num_classes: int = 2, dice: bool
         loss = torch.mean(loss)*0.5
         if dice is True:
             dice_target = build_target(target, num_classes, ignore_index)
-            loss+ = dice_loss(x, dice_target, multiclass=True, ignore_index=ignore_index)*0.5
+            loss += dice_loss(x, dice_target, multiclass=True, ignore_index=ignore_index)*0.5
         losses[name] = loss
 
     if len(losses) == 1:
